31d232a32a2519d0834391205a05765458e1eff7
Ivan Sholana <ivansholana@gmail.com>
2023-07-31 16:33:30 +0700

    done but just text

--- Server.py
+++ Server.py
@@ -14,7 +14,9 @@ sockMulti = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sockMulti.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
 # Inisialisasi socket UDP (BroadCast)
-UDP_IP = '127.0.0.3'  # Ganti dengan alamat IP tujuan unicast yang sesuai
+UDP_IP_1 = '127.0.0.1'  # Ganti dengan alamat IP tujuan unicast yang sesuai
+UDP_IP_2 = '127.0.0.2'  # Ganti dengan alamat IP tujuan unicast yang sesuai
+UDP_IP_3 = '127.0.0.3'  # Ganti dengan alamat IP tujuan unicast yang sesuai
 UDP_PORT = 5006
 broadcast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -31,4 +33,6 @@ while True:
     sockMulti.sendto("Ini Pesan Multicast".encode('utf-8'), (group, port))
 
     # Broadcast
-    broadcast_socket.sendto("Ini Pesan Broadcast".encode(), (UDP_IP, UDP_PORT))
+    broadcast_socket.sendto("Ini Pesan Broadcast".encode(), (UDP_IP_1, UDP_PORT))
+    broadcast_socket.sendto("Ini Pesan Broadcast".encode(), (UDP_IP_2, UDP_PORT))
+    broadcast_socket.sendto("Ini Pesan Broadcast".encode(), (UDP_IP_3, UDP_PORT))
